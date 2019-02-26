6a84d78aef9c49246736521044f44a029d4617be
tdonkor <tdonkor@gmail.com>
2019-02-26 15:17:41 +0000

    Add project files.

---
+++ payment/Loader/payment_ecrutilatl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payment_ecrutilatl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
